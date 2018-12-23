c2545de4292265ae2b0450ec2439ba2e74a1867b
frontend33 <vikulov33@rambler.ru>
2018-12-23 17:16:07 +0300

    Регистрация компонентов и передача параметров в компонент

---
+++ src/Car.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="car">
+      <h3>Name:{{carName}}</h3>
+      <p>Year: {{carYear}}</p>
+    </div>
+</template>
+
+
+<script>
+  export default {
+//  Можно передать параметры в data и передать параметры из другого файла методом props
+    props:[`carName`,`carYear`],
+//    data:function()
+
+
+
+//    data(){
+//      return {
+//        carYear: 2018
+//      }
+//    }
+  }
+
+</script>
+
+<style>
+  .car{
+    border:1px solid black;
+  }
+  .car h3{
+    margin-bottom: 5px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Car from './Car.vue'
+
+//До инициализации подключаем компонент
+Vue.component('app-car',Car)
 
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+
+// render:function(h){
+//  h(app)
+// }
