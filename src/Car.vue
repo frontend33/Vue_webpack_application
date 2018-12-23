d14b96632c2a56f87d0ce2ab4b730dfcc267a0fd
frontend33 <vikulov33@rambler.ru>
2018-12-23 20:57:19 +0300

    Добавил валидаию входящих параметров внутри компонента

--- src/Car.vue
+++ src/Car.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="car">
-      <h3>Name:{{carName}}</h3>
+      <h3>Name:{{carName}} / {{reverseName}}</h3>
       <p>Year: {{carYear}}</p>
     </div>
 </template>
@@ -9,7 +9,22 @@
 <script>
   export default {
 //  Можно передать параметры в data и передать параметры из другого файла методом props
-    props:[`carName`,`carYear`],
+//    props:[`carName`,`carYear`],
+//    Можно передать props как объект
+    props:{
+      carName:{
+        type:String,
+        required:true,
+        default:`defaul Name`
+      },
+//      Либо просто указав тип
+      carYear:Number
+    },
+    computed:{
+      reverseName(){
+        return this.carName.split('').reverse().join('')
+      }
+    }
 //    data:function()
 
 
