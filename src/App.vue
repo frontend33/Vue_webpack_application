7a937d00bd0138fc5e06afec2a1df9b3015e6223
frontend33 <vikulov33@rambler.ru>
2018-12-27 10:36:05 +0300

    Work with Input

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,58 @@
     <h2>TextArea</h2>
     <textarea v-model="textarea">{{textarea}}</textarea>
     <p>{{textarea}}</p>
+
+
+<!-- Все выбранные значения находятся в изначально пустом массиве checkboxSocial-->
+    <label>
+      <input type="checkbox" value="instagram" v-model="checkboxSocial"> Instagram
+    </label>
+
+    <label>
+      <input type="checkbox" value="vk" v-model="checkboxSocial"> Vk
+    </label>
+    <label>
+      <input type="checkbox" value="facebook" v-model="checkboxSocial"> Facebook
+    </label>
+
+    <hr>
+
+    <ul>
+      <li v-for="s in checkboxSocial">{{s}}</li>
+    </ul>
+
+    <hr>
+
+    <!-- В радио кнопках можно выбрать одно значение поэтому в функции data
+      указываем не массив а пустую строку-->
+    <label>
+      <input type="radio" value="instagram" v-model="radioSocial"> Instagram
+    </label>
+
+    <label>
+      <input type="radio" value="vk" v-model="radioSocial"> Vk
+    </label>
+    <label>
+      <input type="radio" value="facebook" v-model="radioSocial"> Facebook
+    </label>
+
+    <hr>
+    <p>{{radioSocial}}</p>
+
+    <!-- Селекты -->
+    <select v-model="social">
+      <option v-for="s in socialList">{{s}}</option>
+    </select>
+    <p>{{social}}</p>
+
+    <input type="text" v-model.number="age">
+    <p>{{age}}</p>
+
   </div>
+
+
+
+
 </template>
 
 <script>
@@ -18,9 +69,20 @@ export default {
   data() {
     return{
       name: "",
-      textarea: 'I am initial text'
+      textarea: 'I am initial text',
+      checkboxSocial: ["vk"],
+      radioSocial: '',
+      socialList:["instagram","vk","facebook"],
+      social:"vk",
+      age: 20
     }
   },
+  watch: {
+      age(val){
+        console.log(val)
+        console.log(typeof val)
+      }
+    }
 
 }
 
