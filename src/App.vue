d1855448317b77847540c5fbf1b58b2be94e2b98
frontend33 <vikulov33@rambler.ru>
2018-12-31 21:52:08 +0700

    add libary vue-resourse send to json-server,methods vue post

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,16 @@
 <template>
-  <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container pt-2">
+    <div class="form-group"> 
+      <label for="name">Car name</label>
+      <input type="text" id="name" class="form-control" v-model.trim="carName">
+    </div>
 
-      <div class="collapse navbar-collapse" >
-        <ul class="navbar-nav">
-          <router-link tag="li" class="nav-link" exact to="/" active-class="active">
-            <a class="nav-link">Home</a>
-          </router-link>
-<!--           <li class="nav-item">
-            <router-link class="nav-link" to="/cars">Cars</router-link>
-          </li> -->
-          <!-- У   <router-link> есть специальный параметр
-           tag="И в него передаем в качестве названия тега который хотим использовать"
-          active-class="active" Для выбора активного класса
-         -->
-          <router-link tag="li" class="nav-link" to="/cars" active-class="active">
-            <a class="nav-link">Cars</a>
-          </router-link>
+    <div class="form-group"> 
+      <label for="year">Car name</label>
+      <input type="text" id="year" class="form-control" v-model.number="carYear">
+    </div>
 
-        <!-- Добавляем еще два роутера с Car 3 и Car 4 -->
-          <router-link tag="li" class="nav-link" to="/car/3" active-class="active">
-            <a class="nav-link">Car 3</a>
-          </router-link>
-          <router-link tag="li" class="nav-link" to="/car/4" active-class="active">
-            <a class="nav-link">Car 4</a>
-          </router-link>
-        </ul>
-      </div>
-    </nav>
-    <!-- Тег который говорит куда нужно рендерить определенный роут отвечает   <router-vue>-->
-    <router-view></router-view>
-    <!-- На месте данного тега будет рендерится  тот компонент который соответствует текущему роуту
-        {Который мы описали в routes.js}-->
+    <div class="btn btn-success" @click="createCar">create Car</div>
   </div>
 </template>
 <script>
@@ -39,9 +18,29 @@ export default {
   // Импортируем только нужные валидатор из библиотеки (взят с api vuelidate)
   data() {
     return {
-
+        carName :"",
+        carYear: 2018
     }
   },
+  methods:{
+    createCar(){
+      const car={
+        name:this.carName,
+        year:this.carYear
+      }
+      //если хотим отправить и сохранить на сервере используем метод post
+      this.$http.post('http://localhost:3000/cars',car).then(response=>{
+          // Что бы получить только нужные значения в объекте возвращаем resoponse.json
+          //Для получения чистых данных с севера используем промис распарсим первый объект response c вызывом метода json
+          return response.json()
+      }).then(newCar=>{
+        // В объекте newCar хранятся данные с сервера
+        console.log(newCar)
+      })
+      //Метод post передает нам промис который нужно обработать
+
+    }
+  }
 }
 
 </script>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,14 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import VueResource from "vue-resource"
 import App from './App.vue'
-// Для регистрации базового роута
-import router from './routes'
 
-// Для того что бы подключить любую библиотеку и набор
-// ее функциональности к Vue js достаточно обратиться к глобальной переменной Vue Vue.use()
-Vue.use(VueRouter)
-
+Vue.use(VueResource)
 
 // Зарегистрировать роутер можно в экземпляре приложения после рендер
 new Vue({
   el: '#app',
   render: h => h(App),
   // router:router
-  router
 })
 
 
